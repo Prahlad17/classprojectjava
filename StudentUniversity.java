6fbdf0a41db3850efcb011aaa87a0bc3cdee44de
Prahlad17 <118454513+Prahlad17@users.noreply.github.com>
2023-07-02 18:41:39 +0530

    Update StudentUniversity.java

--- StudentUniversity.java
+++ StudentUniversity.java
@@ -1,18 +1,32 @@
-import java.util.*;
-public class University {
+import java.util.Scanner;
+
+public class StudentUniversity {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Student[] stud1 = new Student[10]();
-       
-       String USN=in.nextLine();
-       String Name=in.nextLine();
-       int semester=in.nextInt();
-       String branch=in.nextLine();
-       String specialization=in.nextLine();
-       String UGDegree=in.nextLine();
-       UGStudent(USN,Name,semester,branch);
-       PGStudent(USN,Name,specialization,UGDegree);
+        Scanner in1 =new Scanner(System.in);
+        UGStudent[] stud1= new UGStudent[10];
+        Student[] stud2= new PGStudent[10];
+        
+        System.out.println("Data inputs for the UG Students");
+        for (int i = 0; i < 1; i++) {
+            String USN=in.nextLine();
+            String Name=in.nextLine();
+            int semester=in1.nextInt();
+            String branch=in1.nextLine();
+
+           stud1[i]=new UGStudent(USN,Name,semester,branch);
+        }
+        System.out.println("Data inputs for the PG Students");
+        for (int i = 0; i < 1; i++) {
+            String USN=in.nextLine();
+            String Name=in.nextLine();
+            String specialization=in.nextLine();
+            String UGDegree=in.nextLine();
+
+            stud2[i]=new PGStudent(USN,Name,specialization,UGDegree);
+        }
+
+
 
     }
-    
 }
