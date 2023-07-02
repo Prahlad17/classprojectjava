67ec519fa571dd23e04e8a08b88ec82381a90beb
Prahlad17 <118454513+Prahlad17@users.noreply.github.com>
2023-07-02 18:51:51 +0530

    Update StudentUniversity.java

--- StudentUniversity.java
+++ StudentUniversity.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class StudentUniversity {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Scanner in1 =new Scanner(System.in);
         UGStudent[] stud1= new UGStudent[10];
         Student[] stud2= new PGStudent[10];
         
@@ -11,8 +10,8 @@ public class StudentUniversity {
         for (int i = 0; i < 10; i++) {
             String USN=in.nextLine();
             String Name=in.nextLine();
-            int semester=in1.nextInt();
-            String branch=in1.nextLine();
+            int semester=in.nextInt();
+            String branch=in.nextLine();
 
            stud1[i]=new UGStudent(USN,Name,semester,branch);
         }
